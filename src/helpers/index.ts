f6c1ff3d7df4da0ecb8e2dd138dabb05d8c1f848
leandromds <leandro.mds.21@gmail.com>
2023-11-17 07:28:53 -0300

    fix: ts parser error

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,14 +1,13 @@
 import { Article } from '@/types'
 import { v4 as uuid } from 'uuid'
-export const filterArticles = (articles:Article[]): Article[] => {
-  return articles.filter((article: Article) => article.urlToImage && article.description)
-}
 
-export const createArticleId = (articles:Article[]): Article[] => {
-  return articles.map((article: Article) => (
-    {
-      articleId: uuid(),
-      ...article
-    }
-  ))
-}
+export const filterArticles = (articles: Article[]): Article[] => (
+  articles.filter(({ urlToImage, description }) => urlToImage && description)
+)
+
+export const createArticleId = (articles: Article[]): Article[] => (
+  articles.map(article => ({
+    ...article,
+    articleId: uuid()
+  }))
+)
