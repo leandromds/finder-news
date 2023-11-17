f7ec3f0b9666d91ad3d9743b107d42af7c239191
leandromds <leandro.mds.21@gmail.com>
2023-11-17 06:02:40 -0300

    feat: initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,33 @@
 /** @type {import('next').NextConfig} */
+const NextFederationPlugin = require('@module-federation/nextjs-mf')
+
+const remotes = ( isServer,) => ({
+  finderNewsComponents: `finderNewsComponents@http://localhost:3001/_next/static/${ isServer ? 'ssr' : 'chunks' }/remoteEntry.js`,
+})
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  webpack(config, options) {
+    const { isServer } = options
+    config.experiments = { topLevelAwait: true}
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'finderNews',
+        filename: 'static/chunks/remoteEntry.js',
+        remotes: remotes('finderNewsComponents', 3001, isServer),
+        shared: {},
+      }),
+    )
+    return config
+  },
 }
 
 module.exports = nextConfig
